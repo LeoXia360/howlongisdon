d9d1b200a0a00ab3c33bf2cf892ce4b92489804b
Leo Xia <leoxia@wireless-10-145-83-191.public.utexas.edu>
2017-04-01 18:50:16 -0500

    asilgfalisg

--- User.py
+++ User.py
@@ -2,9 +2,9 @@ import smtplib
 
 #ALLOW LESS SECURE APPS ON GOOGLE
 server = smtplib.SMTP('smtp.gmail.com', 587)
-server.starttls()
-server.login("leoxia@utexas.edu", "le0ishgg")
+()server.starttls()
+server.login("leoxia@utexas.edu", "password")
  
 msg = "fyoure gr8!"
 server.sendmail("leoxia@utexas.edu", "schilukuri@utexas.edu", msg)
-server.quit()
+server.quit
