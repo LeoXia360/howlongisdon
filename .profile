8b678436fd5856f3a5ce94b0723adf81f4bd1dd5
chilinahur <schilukuri@utexas.edu>
2017-04-01 21:03:45 -0500

    Understanding email in python

---
+++ .profile
@@ -0,0 +1,1 @@
+export PATH="$PATH:/c/Users/mukundasriram/AppData/Local/Programs/Python/Python36/"
--- User.py
+++ User.py
@@ -2,9 +2,10 @@ import smtplib
 
 #ALLOW LESS SECURE APPS ON GOOGLE
 server = smtplib.SMTP('smtp.gmail.com', 587)
-()server.starttls()
+server.starttls()
 server.login("howlongisdon@gmail.com", "donislong")
  
-msg = "fyoure gr8!"
+subject = 'Subject: Testing Mail\n'
+msg = subject +  'fyoure gr8!'
 server.sendmail("leoxia@utexas.edu", "schilukuri@utexas.edu", msg)
 server.quit
